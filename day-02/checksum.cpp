4df74d0748dbb8e2554c43ae0abdac0ff418512c
Alberto Restifo <alberto.restifo@gmail.com>
2019-01-05 00:24:52 +0100

    Add day-02 part 1 excercise

---
+++ day-02/checksum.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+  ifstream infile("input");
+  string line;
+
+  unordered_map<char, int> linesum;
+  unordered_map<int, int> sum;
+
+  while (getline(infile, line)) {
+    linesum.clear();
+
+    for (auto c : line) {
+      linesum[c]++;
+    }
+
+    unordered_map<int, bool> seen;
+    for (auto t : linesum) {
+      if (t.second > 1 && !seen[t.second]) {
+        seen[t.second] = true;
+        sum[t.second]++;
+      }
+    }
+  }
+
+  // Calculate the file checksum
+  int res = 1;
+  for (auto s : sum) {
+    res *= s.second;
+  }
+
+  cout << "Checksum: " << res << endl;
+}
